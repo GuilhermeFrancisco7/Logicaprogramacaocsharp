c6a5246cf04deea7b49cdfe805a15ee55e36f765
GUILHERME FRANCISCO ALMEIDA <guilherme.falmeida7@senacsp.edu.br>
2025-02-03 17:05:53 -0300

    Ativiade

---
+++ logica09/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logica09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Reutilizar a função Somar
+            int resultado1 = Somar(10, 20);
+            Console.WriteLine("A soma de 10 e 20 é: " + resultado1);
+            
+            // Solicitar ao usuário para inserir dois números
+            Console.WriteLine("digite o primeiro número");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número");
+            int numero2= int.Parse(Console.ReadLine());
+          
+            // Chamr a função Somar e armazenar o resultado
+            int resultado = Somar(numero1, numero2);
+
+            // Exibir o resultado
+            Console.WriteLine(" A soma dos dois números é: "+ resultado);  
+
+            Console.ReadLine();
+        } 
+
+        static int Somar(int num1 , int num2)
+        {
+            return num1 + num2;
+           
+            
+        }
+
+
+    }
+}
---
+++ logica10/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logica10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { // Definindo array de inteiros
+            int[] numeros = { 5, 12, 3, 8, 7, 10, 15 };
+
+            // Valor que queros encontrar
+            int valorProcurado = 8;
+
+            // Chamada para a função de pesquisa sequencial
+            int resultado = PesquisaSequencial(numeros, valorProcurado);
+
+            //Exibindo o resultado
+            if (resultado != -1)
+            {
+                Console.WriteLine($"Valor {valorProcurado} encontrado no índice {resultado}");
+            }
+            else
+            {
+                Console.WriteLine($"Valor {valorProcurado} Não encontrado.");
+            }
+            Console.ReadLine();
+        }
+        // Função para realizar a pesquisa sequencial
+        static int PesquisaSequencial(int[] array, int valor)
+        {
+            // Percorrendo o array
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == valor) 
+                {
+                    return i;  // Retorna o índice onde o valor foi econtrado
+                }
+            }
+            return -1; // retorna -1 se o valor não for encontrado
+        }
+    }
+}
